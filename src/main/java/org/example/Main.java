223b75f43e3ba164b4a4cf76d8b7fc94165e51c8
Ievgenii <eugene.podovinnikov@gmail.com>
2023-10-12 11:56:30 +0200

    solve task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MyQueue myQueue = new MyQueue();
+
+        myQueue.push(1);
+        myQueue.push(2);
+        myQueue.peek();
+        myQueue.pop();
+        myQueue.push(5);
+        System.out.println(myQueue.pop());
+        System.out.println(myQueue.pop());
+        System.out.println(myQueue.pop());
+        System.out.println(myQueue.pop());
+
     }
 }
--- src/main/java/org/example/MyQueue.java
+++ src/main/java/org/example/MyQueue.java
@@ -1,2 +1,63 @@
-package org.example;public class MyQueue {
+package org.example;
+
+import java.util.Stack;
+
+@SuppressWarnings("all")
+class MyQueue {
+
+    private Stack<Integer> stack1 = new Stack();
+    private Stack<Integer> stack2 = new Stack();
+
+    private boolean returnsFirst;
+    private boolean pushToFirst;
+
+
+    public void push(int i) {
+        stack1.push(i);
+    }
+
+    public int pop() {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            return 0;
+        }
+        freeFirst();
+        int first = stack1.pop();
+        tossBack();
+        return first;
+    }
+
+    public int peek() {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            return 0;
+        }
+        freeFirst();
+        int first = stack1.peek();
+        tossBack();
+        return first;
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    public void freeFirst() {
+        while (stack1.size() > 1) {
+            stack2.push(stack1.pop());
+        }
+    }
+
+    public void tossBack() {
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
